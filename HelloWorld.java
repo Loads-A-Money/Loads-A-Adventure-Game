67614cf02a0f5586e8344f0064b4a2816a4edb93
Vinstaal0 <vincent-staaltjens@live.nl>
2017-07-24 16:50:46 +0200

    Create HelloWorld.java
    https://codereview.stackexchange.com/questions/70249/text-based-rpg-game

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class HelloWorld {
+    
+    public static Scanner in = new Scanner(System.in);
+    public static String input = "";
+    
+    public static Boolean con = true;
+
+    public static void main(String []args) {
+        
+        
+        
+        while(con) {
+            
+            if (input.equalsIgnoreCase("quit")) {
+                System.out.println("Exiting game ...");
+                break;
+            } else {
+				System.out.println(input);
+			}
+        }
+    }
+}
